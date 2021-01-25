b05de5b006930ee48e05937828ed7185b1783e58
Danielhpe <danihpe@hotmail.com>
2021-01-25 01:08:06 -0300

    feat: add script arguments and code handlings

--- web-scrapper.py
+++ web-scrapper.py
@@ -30,36 +30,66 @@ class Scrapper:
             return None   
         
     def get(self, url): 
-        return requests.get(url)
+        try:
+            status_code = requests.get(url).status_code
+            if status_code == 200:
+                return requests.get(url)
+            return None
+        except:
+            return None
         
-    def initBS(self, data):
+    def init_BS(self, data):
         return BeautifulSoup(data.text, 'html.parser')
     
     def table_scrapper(self, soup, table_attr_type, table_attr_name):
-        leaderboard_table = soup.find('table', { table_attr_type: table_attr_name })
+        leaderboard_table = soup.find('table', { table_attr_type: table_attr_name })  
+
         table_data = []
         fields = []
+        
+        if leaderboard_table is not None:
+            for tr in leaderboard_table.find_all('tr'):
+                for th in tr.find_all('th'):
+                    fields.append(th.text)
 
-        for tr in leaderboard_table.find_all('tr'):
-            for th in tr.find_all('th'):
-                fields.append(th.text)
-                
-        for tr in leaderboard_table.find_all('tr'):
-            datum = {}
-            for i, td in enumerate(tr.find_all('td')):
-                datum[fields[i]] = ' '.join(td.text.split())
-            if datum:
-                table_data.append(datum)
-                
+            for tr in leaderboard_table.find_all('tr'):
+                datum = {}
+                for i, td in enumerate(tr.find_all('td')):
+                    datum[fields[i]] = ' '.join(td.text.split())
+                if datum:
+                    table_data.append(datum)
+
+            return table_data
+        
+        table_data.append({'message':'NAO FOI POSSIVEL REALIZAR SCRAPPING DA TABELA PASSADA'})
         return table_data
         
     def json_converter(self, table_list):
-        return json.dumps(table_data, indent=4)   
+        return json.dumps(table_list, indent=4)   
      
 scrapper = Scrapper()
-umg_url = scrapper.get_urL('(?<=UGM:)(.*)')
-umg_data = scrapper.get(umg_url)
-soup = scrapper.initBS(umg_data)
-table_data = scrapper.table_scrapper(soup, 'id', 'leaderboard-table')
+table_data = []
+
+url_ref = sys.argv[1]
+
+if url_ref not in scrapper.read('websites.txt'):
+    table_data.append({'message': 'NENHUM ARGUMENTO PASSADO DE UMA URL VALIDA DE "WEBSITES.TXT"'})
+    json_data = scrapper.json_converter(table_data)
+    scrapper.write('extracted_data.json', json_data)
+    exit()
+
+url = scrapper.get_urL(f'(?<={url_ref})(.*)')
+get_data = scrapper.get(url)
+
+if get_data is not None:   
+    soup = scrapper.init_BS(get_data)
+    html_element = sys.argv[2]
+    html_value = sys.argv[3]
+    table_data =  scrapper.table_scrapper(soup, html_element, html_value)
+    json_data = scrapper.json_converter(table_data)
+    scrapper.write('extracted_data.json', json_data)
+    exit()
+    
+table_data.append({'message': 'URL INVÁLIDA OU STATUS CODE DO REQUEST DIFERENTE DE 200 (SUCESSO)'})
 json_data = scrapper.json_converter(table_data)
 scrapper.write('extracted_data.json', json_data)
