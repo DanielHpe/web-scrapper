ff1c165b4d532ad123d4797a1436cd1927416689
Danielhpe <danihpe@hotmail.com>
2021-01-25 22:13:23 -0300

    refac: add python

--- run_scrapper.bat
+++ run_scrapper.bat
@@ -1,4 +1,7 @@
 SET mypath=%~dp0
+REM INSTALA O PYTHON
+%mypath%python\python373.exe /quiet InstallAllUsers=1 TargetDir=C:\Python\Python37 Include_pip=1 Include_test=0 PrependPath=1
+REM PYTHON INSTALDO COM SUCESSO
 REM ADICIONA AS BIBLIOTECAS UTILIZADAS
 C:\Python\Python37\python.exe -m pip install beautifulsoup4
 C:\Python\Python37\python.exe -m pip install requests
